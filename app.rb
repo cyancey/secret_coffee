37ef5f6355d0d5066a1df2259241cdd264b10167
Chris Yancey <cyancey1@gmail.com>
2015-01-10 14:45:30 -0800

    updated secret coffee validation, api, and admin view

--- app.rb
+++ app.rb
@@ -15,7 +15,6 @@ class SecretCoffee < ActiveRecord::Base
   def self.set_coffee_time
     now = Time.now.in_time_zone("Pacific Time (US & Canada)")
     coffee_time = DateTime.new(now.year, now.month, now.day, 13, 0, 0, '-8') + rand(100).minutes
-    # coffee_time = Time.now
     SecretCoffee.create(time: coffee_time, coffee_quote: CoffeeQuote.random)
   end
 
@@ -40,9 +39,13 @@ class SecretCoffee < ActiveRecord::Base
   private
 
   def one_secret_coffee_run_per_day
-    now = Time.now.in_time_zone("Pacific Time (US & Canada)")
-    coffee_runs_today = SecretCoffee.where(time: now.beginning_of_day..now.end_of_day)
+    coffee_time = self.time.in_time_zone("Pacific Time (US & Canada)")
+    coffee_runs_today = SecretCoffee.where(time: coffee_time.beginning_of_day..coffee_time.end_of_day)
     if coffee_runs_today.size > 0
+      puts "Couldn't create new secret coffee run"
+      coffee_runs_today.each do |secret_coffee|
+        p secret_coffee.as_json
+      end
       errors.add(:base, 'You can only do one secret coffee run per day')
     end
   end
@@ -74,12 +77,26 @@ get '/' do
 end
 
 get '/admin' do
-  @secret_coffees = SecretCoffee.all
+  @secret_coffees = SecretCoffee.order(:time)
   haml :admin
 end
 
 get '/api' do
   content_type :json
-  { secret_coffee_time: SecretCoffee.secret_coffee_time? }.to_json
+  if !SecretCoffee.secret_coffee_time?
+    { secret_coffee_time: false }.to_json
+  else
+    now = Time.now.in_time_zone("Pacific Time (US & Canada)")
+    @secret_coffee = SecretCoffee.where(time: now.beginning_of_day..now.end_of_day).last
+    @quote = @secret_coffee.coffee_quote
+    if @quote
+      { secret_coffee_time: true,
+        coffee_quote: {quote: @quote.quote,
+                       said_by: @quote.said_by}}.to_json
+    else
+      { secret_coffee_time: true,
+        coffee_quote: nil}.to_json
+    end
+  end
 end
 
