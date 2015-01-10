40e6ae6fa2865623f7c611758f7d4055cbe984b4
Chris Yancey <cyancey1@gmail.com>
2015-01-09 22:51:16 -0800

    added methods and rake tasks for setting / updating secret coffee models

--- Rakefile
+++ Rakefile
@@ -1,2 +1,31 @@
 require './app'
 require 'sinatra/activerecord/rake'
+
+namespace :secret_coffee do
+
+  desc 'Set secret coffee time for today'
+  task :set_time do
+    SecretCoffee.set_coffee_time
+  end
+
+  desc "Send notification for secret coffee if it hasn't been sent"
+  task :send_notification_if_coffee_time do
+    if SecretCoffee.secret_coffee_time?
+      today = Time.now.in_time_zone("Pacific Time (US & Canada)")
+      todays_coffee_runs = SecretCoffee.where(time: today.beginning_of_day..today.end_of_day)
+
+      notification_sent_for_todays_runs = todays_coffee_runs.map do |secret_coffee|
+        secret_coffee.notification_sent
+      end.include?(true)
+
+      unless notification_sent_for_todays_runs
+        ## send notification
+
+        ## mark all todays runs as having notifications sent
+        todays_coffee_runs.each {|secret_coffee| secret_coffee.update_attributes(notification_sent: true)}
+      end
+
+    end
+  end
+
+end
--- app.rb
+++ app.rb
@@ -1,8 +1,42 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
+require 'active_support/all'
+require 'haml'
 
 
 class SecretCoffee < ActiveRecord::Base
+  validate :one_secret_coffee_run_per_day, on: :create
 
+  def self.set_coffee_time
+    now = Time.now.in_time_zone("Pacific Time (US & Canada)")
+    coffee_time = DateTime.new(now.year, now.month, now.day, 13, 0, 0, '-8') + rand(100).minutes
+    SecretCoffee.create(time: coffee_time)
+  end
+
+  def self.secret_coffee_time?
+    now = Time.now.in_time_zone("Pacific Time (US & Canada)")
+    todays_secret_coffees = SecretCoffee.where(time: now.beginning_of_day..now.end_of_day).to_a
+
+    todays_secret_coffees.map do |secret_coffee|
+      (secret_coffee.time.in_time_zone("Pacific Time (US & Canada)") >= now) && (secret_coffee.time <= (now.in_time_zone("Pacific Time (US & Canada)") + 15.minutes))
+    end.include?(true)
+
+  end
+
+  private
+
+  def one_secret_coffee_run_per_day
+    today = Date.today.in_time_zone("Pacific Time (US & Canada)")
+    coffee_runs_today = SecretCoffee.where(time: today.beginning_of_day..today.end_of_day)
+    if coffee_runs_today.size > 0
+      errors.add(:base, 'You can only do one secret coffee run per day')
+    end
+  end
 end
+
+get '/' do
+  @secret_coffee_time = SecretCoffee.secret_coffee_time?
+  haml :home
+end
+
