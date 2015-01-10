20d6c71d550cc3d90568ff48ae4ab09b7f9af87b
Chris Yancey <cyancey1@gmail.com>
2015-01-09 22:49:52 -0800

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150110030640) do
+ActiveRecord::Schema.define(version: 20150110052445) do
 
   create_table "secret_coffees", force: :cascade do |t|
     t.datetime "time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "notification_sent", default: false
   end
 
 end
